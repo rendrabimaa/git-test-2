d24edc96c291e0dca07794e5b8c622d6c5a1438b
rendrabimaa <rendrabimaa@gmail.com>
2021-11-28 16:03:00 +0700

    Menambahkan Paragraph

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
 <h1> Hello World</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni dicta sed unde quis impedit dignissimos eius odio ut eos quia.</p>
 </body>
 </html>
